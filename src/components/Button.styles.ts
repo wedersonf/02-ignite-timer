2e981c02e2fe6a7c8044153a0f97ec183befbb02
wedersonf <wedymf@hotmail.com>
2023-07-06 01:39:45 -0300

    feat: lint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,33 +1,33 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'red',
-  success: 'green'
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  color: ${props => props.theme.white};
+  color: ${(props) => props.theme.white};
   border-radius: 8px;
 
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme['green-500']};
+  background-color: ${(props) => props.theme['green-500']};
 
-  /* ${props => {
+  /* ${(props) => {
     return css`
-      background-color: ${buttonVariants[props.variant]}
+      background-color: ${buttonVariants[props.variant]};
     `
   }} */
-`;
+`
